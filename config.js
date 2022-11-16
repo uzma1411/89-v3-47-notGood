f7958df798aa9988bb9b034993b4117a5b8fbe59
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2022-11-16 14:52:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyBFzvet2ScDtruJO8nWnXqvjFevf3cv0S0",
+  authDomain: "c85-aaron.firebaseapp.com",
+  databaseURL: "https://c85-aaron-default-rtdb.firebaseio.com",
+  projectId: "c85-aaron",
+  storageBucket: "c85-aaron.appspot.com",
+  messagingSenderId: "728021017251",
+  appId: "1:728021017251:web:9a36f40d6aeef8a3885516"
+  };
+  // Initialize Firebase
+  
+
